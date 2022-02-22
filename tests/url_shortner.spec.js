65d545b0e78c7a2258485aadc7f502fdf4244397
Bernardo Moraes <bernardo.moraes.silva@gmail.com>
2022-02-22 18:08:03 -0300

    test: 🧪 Add failing tests

---
+++ tests/url_shortner.spec.js
@@ -0,0 +1,20 @@
+describe('Url Shortner Use Cases', () => {
+  describe('Shortner', () => {
+    it('should shorten an URL', () => {
+      
+    })
+    it('should crawl title from original url', async () => {
+
+    })
+  })
+  describe('Most Used', () => {
+    it('should return the most used URLs', () => {
+      
+    })
+  })
+  describe('Redirect', () => {
+    it('should redirect to original URL', () => {
+      
+    })
+  })
+})
